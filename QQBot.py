72a87ed239f7a6e3fd7a9e335457c93116c4338e
Chem-Aether <102016380+Chem-Aether@users.noreply.github.com>
2023-06-18 22:54:32 +0800

    Update QQBot.py

--- QQBot.py
+++ QQBot.py
@@ -1,8 +1,11 @@
+import plugins.YD as YD
 import plugins.JWC as JWC
 import plugins.weather as Weather
 import plugins.translate as Translate
 import plugins.WBhot as WB
 import Help
+
+
 
 from flask import Flask, request
 from flask_frozen import Freezer
@@ -31,6 +34,20 @@ def SendMesg(url,text,Mesg_type):
         }
         requests.post(url=url, data=datas)
 
+def CQFace(id):
+    CQ = f'[CQ:face,id={id}]'
+    return CQ
+
+def CQImage(url,type,id):
+    CQ = f'[CQ:image,file={url},type={type},id={id}]'
+    return CQ
+
+def CQMusic(type=163,id=5250079):
+
+    #id = "004Azgn03TGRIk"
+    CQ = f'[CQ:music,type={type},id={id}]'
+    return CQ
+
 def Listener(text):
     try:
         L = text.split('#')
@@ -47,6 +64,40 @@ map = {
 def add(name,fun):
     map[name] = fun
 
+def aaa():
+    url = 'http://127.0.0.1:5700/send_msg'
+    text = '''{
+    "type": "text",
+    "data": {
+        "id": "123"
+    }
+}'''
+    Mesg_type = 'private'
+    print(request.get_json().get('message_type'))
+    if Mesg_type == 'private':
+        print(request.get_json().get('user_id'), type(request.get_json().get('user_id')))
+        datas = {
+            'message_type': str(request.get_json().get('message_type')),
+            'user_id': '3254806570',
+            'message': text
+        }
+        requests.post(url=url, data=datas)
+
+    if Mesg_type == 'group':
+        print(request.get_json().get('group_id'), type(request.get_json().get('group_id')))
+        datas = {
+            'message_type': request.get_json().get('message_type'),
+            'group_id': str(request.get_json().get('group_id')),
+            'message': text
+        }
+        requests.post(url=url, data=datas)
+
+
+
+
+
+
+add('/ah',aaa)
 
 add('/ls',Help.get_Cmd_List)
 add('/help',Help.get_Help)
@@ -58,12 +109,17 @@ add('/nl',Weather.get_Today_Culture)
 add('/jwc',JWC.get_News)
 add('/fy',Translate.translate)
 add('/rs',WB.get_WbHot)
+add('/yd',YD.creat_String)
+
+
+
+add('/yy',CQMusic)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
     Listener(request.get_json().get('message'))
+
     return ''
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=8080)
-#print(plugins.JWC.get_News())
